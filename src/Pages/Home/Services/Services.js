070049f238c3f566ec318ef744df3c6e2186ab58
rupom96 <rupom.cse16@gmail.com>
2021-10-31 22:39:06 +0600

    after adding spinner in home service

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -15,6 +15,15 @@ const Services = () => {
             <h1 id="servicesid">Our Trip Packages:</h1>
             <div className="card-container px-5 mt-5">
 
+                {data.length === 0 &&
+
+                    <div class="d-flex justify-content-center mt-5 mb-5">
+                        <div class="spinner-border text-danger" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                }
+
                 <div className="row row-cols-1 row-cols-md-3 g-4">
                     {/* per card will be set here */}
                     {
